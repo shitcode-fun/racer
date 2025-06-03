18ba0f4049964aea3eae3aa8bfc1f0134446a3cc
ShitCode.fun <spindeez92@gmail.com>
2025-06-03 15:12:12 +0200

    Add src/app/page.tsx from nextjs template

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen items-center justify-center bg-background">
+      {/* Main content here */}
+    </main>
+  );
+}
