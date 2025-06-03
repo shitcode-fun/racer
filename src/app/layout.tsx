8bf967a913f41ac5d8df53033275878204f192c0
ShitCode.fun <spindeez92@gmail.com>
2025-06-03 15:12:11 +0200

    Add src/app/layout.tsx from nextjs template

---
+++ src/app/layout.tsx
@@ -0,0 +1,28 @@
+import type { Metadata } from "next";
+import { GeistSans } from 'geist/font/sans';
+import { GeistMono } from 'geist/font/mono';
+import "./globals.css";
+import { Providers } from './providers';
+import { Navbar } from '../components/Navbar';
+
+export const metadata: Metadata = {
+  title: "Web3 App Template",
+  description: "Minimal Next.js web3 template.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className={`${GeistSans.variable} ${GeistMono.variable}`}>
+      <body className="antialiased">
+        <Providers>
+          <Navbar />
+          {children}
+        </Providers>
+      </body>
+    </html>
+  );
+}
